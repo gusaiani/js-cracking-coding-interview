fea7fd17bd3a02aac5e9c6df6aca5c9fd4061a6e
Gustavo Saiani <gustavo@poe.ma>
2017-04-28 21:21:29 -0300

    Palindrome permutation

---
+++ palindrome_permutations_v1.js
@@ -0,0 +1,37 @@
+/*
+  * Chapter 1
+  * Page 90
+  *
+  *   1.3 Palindrome Permutation
+  *   Given a string, write a function to check if it is
+  *   a permutation of a palindrome.
+ */
+
+String.prototype.isPalindromePermutation = function() {
+  let chars = this.toLowerCase().split(' ').join('').split('');
+  let linkedList = {};
+  let numberOfLettersOccurringEvenTimes = 0;
+
+  for (char of chars) {
+    if (char.toLowerCase() == char.toUpperCase()) { char.ntinue; }
+
+    if (linkedList[char] === undefined) {
+      linkedList[char] = 1;
+    } else {
+      linkedList[char] = undefined;
+    }
+  }
+
+  console.log(linkedList);
+  for (let value in linkedList) {
+    if (linkedList[value] === 1) {
+      numberOfLettersOccurringEvenTimes++;
+    }
+
+    if (numberOfLettersOccurringEvenTimes > 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
