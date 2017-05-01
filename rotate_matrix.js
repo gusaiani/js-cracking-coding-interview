247e31f9624b4e59b4cd73216dd3a61609fcdd79
Gustavo Saiani <gustavo@poe.ma>
2017-05-01 10:36:07 -0300

    Start rotate matrix

---
+++ rotate_matrix.js
@@ -0,0 +1,13 @@
+/*
+ * Chapter 1
+ * Page 91
+ *
+ * 1.7 Given an image represented by an NxN matrix,
+ * where each pixel in the image is 4 bytes,
+ * write a method to rotate the image by 90 degrees.
+ * Can you do this in place?
+ */
+
+Array.prototype.rotate = function() {
+  return this;
+}
---
+++ tests/rotate_matrix.test.js
@@ -0,0 +1,6 @@
+require('../rotate_matrix');
+
+test("[0,0] returns [0,0]", function() {
+  expect([0,0].rotate()).toEqual([0,0]);
+});
+
