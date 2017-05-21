1cf173b8371c442452342b7e6518be14faaceb69
Gustavo Saiani <gustavo@poe.ma>
2017-05-21 08:36:03 +0200

    Check if strings are rotations of one another

--- check_string_rotation.js
+++ check_string_rotation.js
@@ -10,7 +10,33 @@
  */
 
 function isRotation(string1, string2) {
-  return "break the test";
+  if (string1.length !== string2.length) return false;
+
+  const char1 = string1.charAt(0);
+
+  for (let i = 0; i < string1.length; i++) {
+    let char2 = string2.charAt(i);
+
+    if (char1 == char2) {
+      const charsLeftInString2 = string2.length - i - 1;
+
+      for (let j = 1; j < charsLeftInString2; j++) {
+        let nextChar1 = string1.charAt(j);
+        let nextChar2;
+
+        if (j <= charsLeftInString2) {
+          nextChar2 = string2.charAt(i + j);
+        } else {
+          nextChar2 = string2.charAt(i + j - string2.length);
+        }
+
+        if (nextChar1 != nextChar2) break;
+        if (j = string2.length) return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 module.exports = isRotation;
--- tests/check_string_rotation.test.js
+++ tests/check_string_rotation.test.js
@@ -8,6 +8,15 @@ test('"coke" and "pepsi" are not rotations', () => {
   expect(isRotation("coke", "pepsi")).toBe(false);
 });
 
+test('"coke" and "ecok" are rotations', () => {
+  expect(isRotation("coke", "ecok")).toBe(true);
+});
+
 test('"waterbottle" and "erbotlewat" are not rotations', () => {
   expect(isRotation("waterbottle", "erbotlewat")).toBe(false);
 });
+
+test('"watwater" and "erwatwat" are rotations', () => {
+  expect(isRotation("watwater", "erwatwat")).toBe(true);
+});
+
