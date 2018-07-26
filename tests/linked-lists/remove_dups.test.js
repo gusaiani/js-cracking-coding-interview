f34dce29710f44a90c7e286391c4ae6d45fcde0e
Gustavo Saiani <gustavo@poe.ma>
2018-07-25 23:49:19 -0300

    Add another test to removeDups

--- tests/linked-lists/remove_dups.test.js
+++ tests/linked-lists/remove_dups.test.js
@@ -10,6 +10,20 @@ test("should remove LinkedList dupe", function() {
   expect(list.head.next).toBe(null);
 });
 
+test("should remove LinkedList dupe", function() {
+  let list = new LinkedList();
+
+  list.push(3);
+  list.push(3);
+  list.push(4);
+  list.push(5);
+  list.push(6);
+  list.push(4);
+  list.removeDuplicates();
+
+  expect(list.head.next.next.next.next).toBe(null);
+});
+
 test("should keep LinkedList without dupe as is", function() {
   let list = new LinkedList();
 
