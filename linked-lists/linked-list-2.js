2deba3f0db76e7c71454ff19b67ee56e976c04ba
Gustavo Saiani <gustavo@poe.ma>
2018-07-25 23:49:06 -0300

    Clean up second version of SinglyList

--- linked-lists/linked-list-2.js
+++ linked-lists/linked-list-2.js
@@ -1,107 +1,95 @@
 function Node(data) {
-    this.data = data;
-    this.next = null;
+  this.data = data;
+  this.next = null;
 }
 
 function SinglyList() {
-    this._length = 0;
-    this.head = null;
+  this._length = 0;
+  this.head = null;
 }
 
 SinglyList.prototype.add = function(value) {
-    var node = new Node(value),
-        currentNode = this.head;
+  var node = new Node(value),
+      currentNode = this.head;
 
-    // 1st use-case: an empty list
-    if (!currentNode) {
-        this.head = node;
-        this._length++;
-
-        return node;
-    }
-
-    // 2nd use-case: a non-empty list
-    while (currentNode.next) {
-        currentNode = currentNode.next;
-    }
-
-    currentNode.next = node;
-
+  // 1st use-case: an empty list
+  if (!currentNode) {
+    this.head = node;
     this._length++;
 
     return node;
+  }
+
+  // 2nd use-case: a non-empty list
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = node;
+
+  this._length++;
+
+  return node;
 };
 
 SinglyList.prototype.searchNodeAt = function(position) {
-    var currentNode = this.head,
-        length = this._length,
-        count = 1,
-        message = {failure: 'Failure: non-existent node in this list.'};
+  var currentNode = this.head,
+      length = this._length,
+      count = 1,
+      message = {failure: 'Failure: non-existent node in this list.'};
 
-    // 1st use-case: an invalid position
-    if (length === 0 || position < 1 || position > length) {
-        throw new Error(message.failure);
-    }
+  // 1st use-case: an invalid position
+  if (length === 0 || position < 1 || position > length) {
+    throw new Error(message.failure);
+  }
 
-    // 2nd use-case: a valid position
-    while (count < position) {
-        currentNode = currentNode.next;
-        count++;
-    }
+  // 2nd use-case: a valid position
+  while (count < position) {
+    currentNode = currentNode.next;
+    count++;
+  }
 
-    return currentNode;
+  return currentNode;
 };
 
 SinglyList.prototype.remove = function(position) {
-    var currentNode = this.head,
-        length = this._length,
-        count = 1,
-        message = {failure: 'Failure: non-existent node in this list.'},
-        beforeNodeToDelete = null,
-        nodeToDelete = null,
-        deletedNode = null;
+  var currentNode = this.head,
+      length = this._length,
+      count = 1,
+      message = {failure: 'Failure: non-existent node in this list.'},
+      beforeNodeToDelete = null,
+      nodeToDelete = null,
+      deletedNode = null;
 
-    // 1st use-case: an invalid position
-    if (position < 0 || position > length) {
-        throw new Error(message.failure);
-    }
+  // 1st use-case: an invalid position
+  if (position < 0 || position > length) {
+    throw new Error(message.failure);
+  }
 
-    // 2nd use-case: the first node is removed
-    if (position === 1) {
-        this.head = currentNode.next;
-        deletedNode = currentNode;
-        currentNode = null;
-        this._length--;
-
-        return deletedNode;
-    }
-
-    // 3rd use-case: any other node is removed
-    while (count < position) {
-        beforeNodeToDelete = currentNode;
-        nodeToDelete = currentNode.next;
-        currentNode = currentNode.next;
-        count++;
-    }
-
-    beforeNodeToDelete.next = nodeToDelete.next;
-    deletedNode = nodeToDelete;
-    nodeToDelete = null;
+  // 2nd use-case: the first node is removed
+  if (position === 1) {
+    this.head = currentNode.next;
+    deletedNode = currentNode;
+    currentNode = null;
     this._length--;
 
     return deletedNode;
+  }
+
+  // 3rd use-case: any other node is removed
+  while (count < position) {
+    beforeNodeToDelete = currentNode;
+    nodeToDelete = currentNode.next;
+    currentNode = currentNode.next;
+    count++;
+  }
+
+  beforeNodeToDelete.next = nodeToDelete.next;
+  deletedNode = nodeToDelete;
+  nodeToDelete = null;
+  this._length--;
+
+  return deletedNode;
 };
 
-a = new SinglyList()
-
-for (let i = 1; i < 9; i++) {
-  a.add(i)
-}
-
-const util = require('util')
-console.log(util.inspect(a, false, null))
-console.log('-'.repeat(15))
-a.remove(3)
-console.log(util.inspect(a, false, null))
-
-// module.exports = SinglyList;
+module.exports = SinglyList;
